38508b607571cb58b951a620bf237a20c80aedd5
James Townley <software@peachyprinter.com>
2016-01-21 12:43:56 -0600

    Start work on api

--- src/firmware.py
+++ src/firmware.py
@@ -1,9 +1,7 @@
-class FirmwareChecker(object):
-    def init(self):
-        pass
-
-    def get_avaliable_versions(self, hardware_version):
-        pass
+import os
+from subprocess import Popen, PIPE
+import logging
+logger = logging.getLogger('peachy')
 
 
 class FirmwareUpdater(object):
@@ -18,15 +16,44 @@ class FirmwareUpdater(object):
 
 
 class WindowsFirmwareUpdater(FirmwareUpdater):
-    def init(self):
-        pass
+    def __init__(self, dependancy_path, peachy_printer_address='0483:df11'):
+        self.dependancy_path = dependancy_path
+        self.usb_address = peachy_printer_address
+        self.dfu_exe = os.path.join(self.dependancy_path, 'dfu-util-static.exe')
 
     def check_ready(self):
-        pass
+        process = Popen([self.dfu_exe, '-l'], stdout=PIPE, stderr=PIPE)
+        (out, err) = process.communicate()
+        process.wait()
+        return self.usb_address in err
 
     def update(self, firmware_path):
-        pass
+        process = Popen([
+            self.dfu_exe,
+            '-a', '0',
+            '--dfuse-address', '0x08000000',
+            '-D', firmware_path,
+            '-v',
+            '-d', self.usb_address
+            ], stdout=PIPE, stderr=PIPE)
+        (out, err) = process.communicate()
+        exit_code = process.wait()
+        if exit_code != 0:
+            logger.error("Output: {}".format(out))
+            logger.error("Error: {}".format(err))
+            logger.error("Exit Code: {}".format(exit_code))
+            raise Exception('Failed to update device')
 
 
 def get_firmware_updater(self):
     return WindowsFirmwareUpdater()
+
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level='INFO')
+path = os.path.join('dependancies', 'windows')
+wfu = WindowsFirmwareUpdater(path)
+
+firmware = '..\\peachyprinter-firmware-0.1.180.bin'
+if wfu.check_ready():
+    wfu.update(firmware)
+
