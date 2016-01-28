c3e5fe9580da3e824f31058db50d1f6087457ea0
James Townley <software@peachyprinter.com>
2016-01-28 13:55:09 -0600

    Building python build script

---
+++ src/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _Install
+from VERSION import version
+import os
+import glob
+
+
+setup(
+    name='PeachyPrinterFirmwareAPI',
+    version=version,
+    description='Tool for updating the firmware of peachyprinter',
+    options={},
+    url="http://www.peachyprinter.com",
+    author="Peachy Printer",
+    author_email="software+peachyprintertools@peachyprinter.com",
+    package_data={'': ['*', 'peachyprinter/dependancies/windows/*'],
+                  '': ['*', 'peachyprinter/dependancies/mac/*'],
+                  '': ['*', 'peachyprinter/dependancies/linux/*'],
+                 },
+    install_requires=[''],
+    packages=find_packages(),
+    py_modules=['VERSION'],
+    include_package_data=True
+      )
+
+class install(_Install):
+    def run(self):
+        super(install, self).run(self)
+        print "BADA-BADA-KABONG"
