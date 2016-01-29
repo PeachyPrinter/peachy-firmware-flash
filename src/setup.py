e0d2ff9a0d670d215fa92aa41e5a96f4bb2b4a42
James Townley <software@peachyprinter.com>
2016-01-29 11:47:08 -0600

    Actually including the software in the pacakage

--- src/setup.py
+++ src/setup.py
@@ -16,7 +16,7 @@ setup(
                   },
     install_requires=[''],
     packages=find_packages(),
-    py_modules=['VERSION'],
+    py_modules=['firmware'],
     include_package_data=True
       )
 
