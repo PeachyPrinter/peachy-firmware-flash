32ac8edaa7abc73288327f158cb52c12b447ef04
William Topping <William Topping>
2016-02-05 10:04:13 -0600

    Better organisation and added a failure mode version

---
+++ src/helperC/Driver_mockup/wdi-simple-mock.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("BOOTLOADER,DRIVER:STT30USB,VERSION:34556123\n");
+	printf("BOOTLOADER,DRIVER:STT30USB,VERSION:34556123\n");
+	printf("RETURN:-4,No such device\n");
+	
+	return 0;
+}
