557b58682dece82fadf7f131620b99d6db507761
William Topping <William Topping>
2016-02-05 09:53:37 -0600

    Adding Successful driver_mockup.exe example

--- src/helperC/wdi-simple-mock.c
+++ src/helperC/wdi-simple-mock.c
@@ -4,7 +4,7 @@ int main(){
 
 	printf("BOOTLOADER,DRIVER:STT30USB,VERSION:34556123\n");
 	printf("BOOTLOADER,DRIVER:WINUSB,VERSION:1234556\n");
-	printf("RETURN:0\n");
+	printf("RETURN:0,Successful Operation\n");
 	
 	return 0;
 }
