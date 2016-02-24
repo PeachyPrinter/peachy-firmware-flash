8a195bdb03c344ec2cc9a809c700b90653fb0913
james townley <james@townley.ca>
2016-02-24 13:06:08 -0600

    Accounting for paths in frozen apps

--- src/firmware/firmware.py
+++ src/firmware/firmware.py
@@ -177,15 +177,23 @@ class WindowsFirmwareUpdater(FirmwareUpdater):
 
 
 def get_firmware_updater(logger=None, bootloader_idvendor=0x0483, bootloader_idproduct=0xdf11, peachy_idvendor=0x16d0, peachy_idproduct=0x0af3):
-    path = os.path.dirname(os.path.abspath(__file__))
     if 'darwin' in sys.platform:
-        dependancies_path = os.path.join(path, 'dependancies', 'mac')
+        if getattr(sys, 'frozen', False):
+            dependancies_path = sys._MEIPASS
+        else:
+            dependancies_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dependancies', 'mac')
         return MacFirmwareUpdater(dependancies_path, bootloader_idvendor, bootloader_idproduct, peachy_idvendor, peachy_idproduct, logger)
     elif 'win' in sys.platform:
-        dependancies_path = os.path.join(path, 'dependancies', 'windows')
+        if getattr(sys, 'frozen', False):
+            dependancies_path = sys._MEIPASS
+        else:
+            dependancies_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dependancies', 'windows')
         return WindowsFirmwareUpdater(dependancies_path, bootloader_idvendor, bootloader_idproduct, peachy_idvendor, peachy_idproduct, logger)
     elif 'linux' in sys.platform:
-        dependancies_path = os.path.join(path, 'dependancies', 'linux')
+        if getattr(sys, 'frozen', False):
+            dependancies_path = sys._MEIPASS
+        else:
+            dependancies_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dependancies', 'linux')
         return LinuxFirmwareUpdater(dependancies_path, bootloader_idvendor, bootloader_idproduct, peachy_idvendor, peachy_idproduct, logger)
     else:
         if logger:
