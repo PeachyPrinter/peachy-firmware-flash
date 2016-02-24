2eb90247a14ef90a28ea5b5d6dc5a5b5c8aa333e
james townley <james@townley.ca>
2016-02-24 15:15:25 -0600

    Fixing a duplicate firmware fetching class and makeing logging real

--- src/firmware/__init__.py
+++ src/firmware/__init__.py
@@ -8,14 +8,13 @@ from firmware import MacFirmwareUpdater, LinuxFirmwareUpdater, WindowsFirmwareUp
 logger = logging.getLogger('peachy')
 
 def get_firmware_updater(bootloader_idvendor=0x0483, bootloader_idproduct=0xdf11, peachy_idvendor=0x16d0, peachy_idproduct=0x0af3):
-    logger.info("Firmware Flash Is Frozen: {}".format(str(getattr(sys, 'frozen', False))))
+    print("Firmware Flash Is Frozen: {}".format(str(getattr(sys, 'frozen', False))))
     if 'darwin' in sys.platform:
         if getattr(sys, 'frozen', False):
             dependancies_path = sys._MEIPASS
         else:
             dependancies_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dependancies', 'mac')
-        if logger:
-            logger.info("Firmware Flash Dependancies Path: {}".format(dependancies_path))
+        print("Firmware Flash Dependancies Path: {}".format(dependancies_path))
         return MacFirmwareUpdater(dependancies_path, bootloader_idvendor, bootloader_idproduct, peachy_idvendor, peachy_idproduct)
     elif 'win' in sys.platform:
         if getattr(sys, 'frozen', False):
